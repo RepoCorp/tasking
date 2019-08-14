018f67e5ab073238483fcf5c5c9b5ba5b49083ab
Catalina De la cuesta <delacuesta@hotmail.com>
2019-08-14 17:26:31 -0500

    Add linter and start with App

--- client/src/App.js
+++ client/src/App.js
@@ -1,50 +1,45 @@
-import React from 'react';
-import {
-    Container,
-    Divider,
-    Dropdown,
-    Grid,
-    Header,
-    Image,
-    List,
-    Menu,
-    Segment,
-} from 'semantic-ui-react'
+import React from "react";
+import { Container, Grid, Image, Menu, Segment } from "semantic-ui-react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import logo from './logo.svg';
-import Home from './Home'
-import Vehicles from './Vehicles'
+import logo from "./logo.svg";
+import Home from "./Home";
+import Vehicles from "./Vehicles";
 
 function App() {
-    return (
-        <Router>
-            <div>
-                <Menu fixed='top' inverted>
-                    <Container>
-                        <Menu.Item as={Link} header to='/'>
-                            <Image size='mini' src={logo} style={{ marginRight: '1.5em' }} />
-                            TasKing
-                        </Menu.Item>
-                        <Menu.Item as={Link} to='/vehiculos'>Vehículos</Menu.Item>
-                    </Container>
-                </Menu>
+  return (
+    <Router>
+      <div>
+        <Menu fixed="top" inverted>
+          <Container>
+            <Menu.Item as={Link} header to="/">
+              <Image size="mini" src={logo} style={{ marginRight: "1.5em" }} />
+              TasKing
+            </Menu.Item>
+            <Menu.Item as={Link} to="/vehiculos">
+              Vehículos
+            </Menu.Item>
+          </Container>
+        </Menu>
 
-                <Grid>
-                    <Grid.Row text style={{ marginTop: '7em' }}>
-                        <Container>
-                            <Route exact path="/" component={Home} />
-                            <Route path="/vehiculos" component={Vehicles} />
-                        </Container>
-                    </Grid.Row>
-                </Grid>
+        <Grid>
+          <Grid.Row text style={{ marginTop: "7em" }}>
+            <Container>
+              <Route exact path="/" component={Home} />
+              <Route path="/vehiculos" component={Vehicles} />
+            </Container>
+          </Grid.Row>
+        </Grid>
 
-                <Segment inverted vertical style={{ margin: '5em 0em 0em', padding: '5em 0em' }}>
-                    <Container textAlign='center'>
-                    </Container>
-                </Segment>
-            </div>
-        </Router>
-    );
+        <Segment
+          inverted
+          vertical
+          style={{ margin: "5em 0em 0em", padding: "5em 0em" }}
+        >
+          <Container textAlign="center" />
+        </Segment>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
